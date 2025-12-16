0a576d165d1790e345bb716ae3a4b4f2e26a1eed
aline <alinefoliveira199@gmail.com>
2025-12-16 08:51:36 -0300

    oi
    oi

---
+++ html/header.php
@@ -0,0 +1,19 @@
+<header class="topo" id="topo">
+    <div class="logo">
+      <img src="../imagens/logo.png" alt="Logo Infinito Particular">
+      <h1>Infinito<br><span>Particular</span></h1>
+    </div>
+  
+    <div class="menu-toggle" id="menuToggle">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  
+    <nav id="nav">
+      <ul>
+        <li><a href="sobre.html">Sobre</a></li>
+        <li><a href="contato.html">Contato</a></li>
+      </ul>
+    </nav>
+  </header>
--- html/index.php
+++ html/index.php
@@ -9,25 +9,7 @@
 </head>
 
 <body>
-  <header class="topo" id="topo">
-    <div class="logo">
-      <img src="../imagens/logo.png" alt="Logo Infinito Particular">
-      <h1>Infinito<br><span>Particular</span></h1>
-    </div>
-
-    <div class="menu-toggle" id="menuToggle">
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-
-    <nav id="nav">
-      <ul>
-        <li><a href="sobre.html">Sobre</a></li>
-        <li><a href="contato.html">Contato</a></li>
-      </ul>
-    </nav>
-  </header>
+  <?php include 'header.php'; ?>
 
   <main>
     <section class="intro fade-in">
